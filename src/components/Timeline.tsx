2e3eaf3ebb3b2e0785dab8e59ea2ab25988d58e1
Albert Huang <ahuang941354@gmail.com>
2025-07-28 22:21:18 -0400

    Fixed timeline not showing up on mobile

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -102,11 +102,11 @@ const Timeline = () => {
 
         <div className="grid gap-4 mx-4 sm:grid-cols-12">
           <div className="relative col-span-12 px-4 space-y-6">
-            <div className="col-span-12 space-y-12 relative px-4 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-[#E8E0DA]">
+            <div className="col-span-12 space-y-12 relative px-4 sm:space-y-8 before:absolute before:top-2 before:bottom-0 before:w-0.5 before:-left-3 before:bg-[#E8E0DA]">
               {timelineData.map((item, index) => (
                 <div
                   key={index}
-                  className={`flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] ${
+                  className={`flex flex-col relative before:absolute before:top-2 before:w-4 before:h-4 before:rounded-full before:left-[-35px] before:z-[1] ${
                     item.type === "work"
                       ? "before:bg-[#2F4156]"
                       : item.type === "education"
